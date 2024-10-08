61b29770c7e22933241a2ba636c2f7bbece617a3
KelDeroS <a.lukin2k4@gmail.com>
2024-10-08 17:37:21 +0300

    Added button to download changed JSON file

--- src/components/TheTable.js
+++ src/components/TheTable.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import data from './table.json'; 
+import data from './table.json';
 import './style.css';
 
 export const DataTable = () => {
   const [tablesData, setTablesData] = useState([]);
   const [tablesHeaders, setTablesHeaders] = useState([]);
+  const [jsonCopy, setJsonCopy] = useState(JSON.parse(JSON.stringify(data)));
 
   useEffect(() => {
     const extractedTablesData = [];
     const extractedTablesHeaders = [];
 
-    data.partitionList.forEach((partition) => {
+    jsonCopy.partitionList.forEach((partition) => {
       partition.detailsSet.forEach((details) => {
         const extractedData = [];
         const headerList = details.neoHeaderList || [];
@@ -22,7 +23,7 @@ export const DataTable = () => {
           const column = header.headerColumn;
           const rowSpan = header.rowSpan;
           const colSpan = header.colSpan;
-          
+
           if (!headerGrid[row]) {
             headerGrid[row] = [];
           }
@@ -38,16 +39,18 @@ export const DataTable = () => {
           headerGrid[row][column] = {
             value: header.value,
             colSpan,
-            rowSpan
+            rowSpan,
+            index: header.index
           };
         })
 
-        console.log(headerGrid);
+        //console.log(headerGrid);
 
         details.rowList.forEach(row => {
           let tableRow = {
             rowName: row.name,
-            rowId: row.rowNumber,
+            rowNumber: row.rowNumber,
+            rowId: row.id,
           };
           let columnNames = [];
 
@@ -84,82 +87,116 @@ export const DataTable = () => {
 
     setTablesData(extractedTablesData);
     setTablesHeaders(extractedTablesHeaders);
-  }, []);
+  }, [jsonCopy]);
 
   const handleInputChange = (tableIndex, rowIndex, event, columnName) => {
     const newData = [...tablesData[tableIndex]];
-    newData[rowIndex][columnName] = event.target.value;
+    const newValue = event.target.value;
+    newData[rowIndex][columnName] = newValue;
+
+    jsonCopy.partitionList.forEach(partition => {
+      partition.detailsSet.forEach(details => {
+        details.rowList.forEach(row => {
+          console.log(row.id, newData[rowIndex].rowId);
+          if (row.id === newData[rowIndex].rowId) {
+            row.indicatorList.forEach(indicator => {
+              if (indicator.name === columnName) {
+                indicator.value = newValue;
+              }
+            });
+          }
+        });
+      });
+    });
+
     setTablesData(prev => {
       const updatedTables = [...prev];
       updatedTables[tableIndex] = newData;
       return updatedTables;
     });
+
+    setJsonCopy(JSON.parse(JSON.stringify(jsonCopy)));
+  }
+
+  const downloadJSON = () => {
+    const json = JSON.stringify(jsonCopy, null, 2); // Форматируем JSON
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'updatedData.json'; // Имя файла
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url); // Освобождаем память
   };
 
   const renderTable = (tableData, headerGrid, tableIndex) => (
-    <table>
-      <thead>
+      <table>
+        <thead>
         {headerGrid.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((header, colIndex) => {
-              if (header !== undefined) {
-                if (header !== '#') {
+            <tr key={rowIndex}>
+              {row.map((header, colIndex) => {
+                if (header !== undefined) {
+                  if (header !== '#') {
+                    return (
+                        <th
+                            key={header.index}
+                            colSpan={header.colSpan}
+                            rowSpan={header.rowSpan}
+                        >
+                          {header.value}
+                        </th>
+                    )
+                  }
+                } else {
                   return (
-                    <th
-                      key={colIndex}
-                      colSpan={header.colSpan}
-                      rowSpan={header.rowSpan}
-                    >
-                      {header.value}
-                    </th>
+                      <th key={colIndex}></th>
                   )
                 }
-              } else {
-                return (
-                  <th key={colIndex}></th>
-                )
-              }
-            })}
-          </tr>
+              })}
+            </tr>
         ))}
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         {tableData.map((item, rowIndex) => (
-          <tr key={rowIndex}>
-            <td>{item.rowName}</td>
-            <td>{item.rowId}</td>
-            {item.columnNames.map((columnName, nullIndex) => (
-              <td key={nullIndex}>
-                {(() => {
-                  if (item[columnName] !== undefined) {
-                    return (
-                      <input
-                        type="text"
-                        value={item[columnName] || ''}
-                        onChange={(event) => handleInputChange(tableIndex, rowIndex, event, columnName)}
-                      />
-                    );
-                  } else {
-                    return <span></span>;
-                  }
-                })()}
-              </td>
-            ))}
-          </tr>
+            <tr key={rowIndex}>
+              <td>{item.rowName}</td>
+              <td>{item.rowNumber}</td>
+              {item.columnNames.map((columnName, nullIndex) => (
+                  <td key={nullIndex}>
+                    {(() => {
+                      if (item[columnName] !== undefined) {
+                        return (
+                            <input
+                                type="text"
+                                value={item[columnName] || ''}
+                                onChange={(event) => handleInputChange(tableIndex, rowIndex, event, columnName)}
+                            />
+                        );
+                      } else {
+                        return <span></span>;
+                      }
+                    })()}
+                  </td>
+              ))}
+            </tr>
         ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
   );
 
   return (
-    <div>
-      {tablesData.map((tableData, tableIndex) => (
-        <div key={tableIndex}>
-          <h2>Таблица {tableIndex + 1}</h2>
-          {renderTable(tableData, tablesHeaders[tableIndex], tableIndex)}
-        </div>
-      ))}
-    </div>
+      <div>
+        {tablesData.map((tableData, tableIndex) => (
+            <div key={tableIndex}>
+              <h2>Таблица {tableIndex + 1}</h2>
+              {renderTable(tableData, tablesHeaders[tableIndex], tableIndex)}
+            </div>
+        ))}
+        <button onClick={downloadJSON}>Скачать JSON</button>
+      </div>
   );
 };
 
